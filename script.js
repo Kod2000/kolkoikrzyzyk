5520b23497be46fe7c2180a9d23d0882bb0b2b32
Jakub Kasprzyk <kasprzyk.jakub789@gmail.com>
2023-01-06 13:36:21 +0100

    Wyświetlanie kółka albo krzyżyka

--- script.js
+++ script.js
@@ -4,7 +4,22 @@ var isGameActive = false;
 var table = [];
 var xOrO //x-false o-true
 function onFieldClick(event) {
+    var row = event.srcElement.dataset.row
+    var col = event.srcElement.dataset.col
+    var result = isFieldEmpty(row, col)
+    if (result == true) {
+        if (xOrO == false) {
+            table[row][col] = false
+            event.srcElement.innerHTML = 'x'
+        } else {
+            table[row][col] = true
+            event.srcElement.innerHTML = 'o'
+        }
+    } else {
+        alert('Tu już coś jest')
+    }
     console.log(event)
+
 }
 
 function newGame() {
@@ -45,6 +60,20 @@ document.addEventListener('DOMContentLoaded', function (event) {
     var game2 = document.getElementById('game')
     game2.style.display = 'none'
 })
+function isFieldEmpty(row, col) {
+    var field = table[row][col]
+    if (field == undefined)
+        return true
+    else return false
+}
+function initFieldHandlers() {
+    var gameFields = document.getElementsByTagName('td')
+    for (var i = 0; gameFields.length > i; i++) {
+        gameFields[i].addEventListener('click', onFieldClicked);
+    }
+}
+
+
 
 
 
